5e98da4377551b54dd618e610f8dc2d680c695c6
Hazem010h <hazem010h@gmail.com>
2023-08-27 19:51:27 +0300

    upload image when user register

--- lib/cubit/register_cubit.dart
+++ lib/cubit/register_cubit.dart
@@ -4,7 +4,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
-import '../constants.dart';
 import '../models/user_model.dart';
 import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 
@@ -72,6 +71,8 @@ class RegisterCubit extends Cubit<RegisterStates>{
       email: email,
       phone: phone,
       image: uploadedProfileImageLink,
+      cover:'',
+      bio: '',
     );
 
     FirebaseFirestore.instance
