300254de7a0f7c17ce28a01640adcb1536bd59f1
mohamedNaser1212 <nasergamal33e@gmail.com>
2023-09-26 22:48:58 +0300

    News details screen done

--- lib/screens/news_screen/news_details_screen.dart
+++ lib/screens/news_screen/news_details_screen.dart
@@ -1,5 +1,5 @@
-import 'package:alex_uni_new/models/news_model.dart';
 import 'package:flutter/material.dart';
+import 'package:alex_uni_new/models/news_model.dart';
 
 class NewsDetailsScreen extends StatefulWidget {
   const NewsDetailsScreen({super.key, required this.newsModel});
@@ -11,69 +11,157 @@ class NewsDetailsScreen extends StatefulWidget {
 }
 
 class _NewsDetailsScreenState extends State<NewsDetailsScreen> {
-
   int index = 0;
+  List<GlobalKey> leftIconButtonKeys = [];
+  List<GlobalKey> rightIconButtonKeys = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Initialize unique keys for each IconButton
+    for (int i = 0; i < widget.newsModel.images!.length; i++) {
+      leftIconButtonKeys.add(GlobalKey());
+      rightIconButtonKeys.add(GlobalKey());
+    }
+  }
+
+  void goToPreviousImage() {
+    if (index > 0) {
+      setState(() {
+        index--;
+      });
+    }
+  }
+
+  void goToNextImage() {
+    if (index < widget.newsModel.images!.length - 1) {
+      setState(() {
+        index++;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    final hasPreviousImage = index > 0;
+    final hasNextImage = index < widget.newsModel.images!.length - 1;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.newsModel.title!),
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            // Container(
-            //   padding: const EdgeInsets.all(10.0),
-            //   height: MediaQuery.of(context).size.height * 0.25,
-            //   child: Image.network(newsModel.headlineImage!),
-            // ),
-            // const SizedBox(
-            //   height: 10.0,
-            // ),
-            // Text(newsModel.headline!, style: const TextStyle(fontSize: 20.0)),
-            // const SizedBox(
-            //   height: 10.0,
-            // ),
-            GestureDetector(
-              onHorizontalDragEnd: (details) {
-                if (details.velocity.pixelsPerSecond.dx > 0) {
-                  if (index > 0) {
-                    setState(() {
-                      index--;
-                    });
-                  }
-                } else if (details.velocity.pixelsPerSecond.dx < 0) {
-                  if (index < widget.newsModel.images!.length - 1) {
-                    setState(() {
-                      index++;
-                    });
-                  }
-                }
-              },
-              child: Column(
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(10.0),
-                    height: MediaQuery.of(context).size.height * 0.25,
-                    child: Image.network(widget.newsModel.images![index]!),
+      body: Column(
+        children: [
+          Expanded(
+            child: Stack(
+              children: [
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: MediaQuery.of(context).size.height * 0.3,
+                    child: Stack(
+                      children: [
+                        Image.network(
+                          widget.newsModel.images[index]!,
+                          width: double.infinity,
+                        ),
+                        if (hasPreviousImage)
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: InkWell(
+                              key: leftIconButtonKeys[index],
+                              onTap: goToPreviousImage,
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Color(0xff2238455E).withOpacity(0.9),
+                                  borderRadius: BorderRadius.circular(30.0),
+                                ),
+                                child: IconButton(
+                                  onPressed: goToPreviousImage,
+                                  icon: Icon(
+                                    Icons.arrow_back_ios_new,
+                                    color: Color(0xffFFFFFF),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        if (hasNextImage)
+                          Align(
+                            alignment: Alignment.centerRight,
+                            child: InkWell(
+                              key: rightIconButtonKeys[index],
+                              onTap: goToNextImage,
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Color(0xff2238455E).withOpacity(0.9),
+                                  borderRadius: BorderRadius.circular(30.0),
+                                ),
+                                child: IconButton(
+                                  onPressed: goToNextImage,
+                                  icon: Icon(
+                                    Icons.arrow_forward_ios,
+                                    color: Color(0xffFFFFFF),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                      ],
+                    ),
                   ),
-                  const SizedBox(
-                    height: 10.0,
+                ),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Container(
+                    clipBehavior: Clip.antiAliasWithSaveLayer,
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 15,
+                          offset: const Offset(0, 3),
+                        ),
+                      ],
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30.0),
+                        topRight: Radius.circular(30.0),
+                      ),
+                    ),
+                    height: MediaQuery.of(context).size.height * 0.65,
+                    width: double.infinity,
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const SizedBox(
+                            height: 10.0,
+                          ),
+                          Text(
+                            widget.newsModel.imageDescription[index]!,
+                            style: const TextStyle(
+                              fontSize: 20.0,
+                            ),
+                            textAlign: TextAlign.start,
+                          ),
+                          const SizedBox(
+                            height: 10.0,
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
-                  Text(
-                    widget.newsModel.imageDescription![index]!,
-                    style: const TextStyle(fontSize: 20.0),
-                  ),
-                  const SizedBox(
-                    height: 10.0,
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
